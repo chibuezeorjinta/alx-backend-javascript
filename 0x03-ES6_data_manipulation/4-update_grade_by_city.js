fc0ac506a6e24e66456e0001911f42fb6f871057
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-04 12:30:51 +0100

    eslint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 const updateStudentGradeByCity = (objList, city, newGrades) => {
   const studentsInCity = objList.filter((obj) => obj.location === city);
   const gradedStudents = studentsInCity.map((student) => {
-		const out = newGrades.find((item) => student.id === item.studentId);
-		const gradeStudent = {
-			...student,
-			grade: 'N/A'
-		}
-		if (out) {
-			gradeStudent.grade = out.grade
-		}
-		return gradeStudent;
+    const out = newGrades.find((item) => student.id === item.studentId);
+    const gradeStudent = {
+      ...student,
+      grade: 'N/A',
+    };
+    if (out) {
+      gradeStudent.grade = out.grade;
+    }
+    return gradeStudent;
   });
-	return gradedStudents;
+  return gradedStudents;
 };
 
 export default updateStudentGradeByCity;
