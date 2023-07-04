ba8d6e114dcb48d9e6412dc75ef9ca8d1afda5ed
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-04 13:18:38 +0100

    account for edge cases of invalid start string inpust

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(inSet, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
   const slicedStings = Array.from(inSet)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
