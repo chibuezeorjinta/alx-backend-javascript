c2049cfc22cc30559f30c3561005dd1c63da9b50
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-04 12:45:26 +0100

    task 4 and 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  const newArr = new ArrayBuffer(length);
+  const int8Edit = new Int8Array(newArr);
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+  int8Edit[position] = value;
+  return newArr;
+}
