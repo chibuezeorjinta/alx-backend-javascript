85a010b32c53499f0a7d67bb6c6e2b15ea6d3313
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-07 14:19:26 +0100

    mix up with parenthesis, causing the then header not to function as a function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,9 @@ export default function handleProfileSignup() {
   const action1 = uploadPhoto();
   const action2 = createUser();
 
-  return Promise.all([action1, action2].then((value) => {
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-  }))
+  return Promise.all([action1, action2])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
     .catch(() => { console.log('Signup system offline'); });
 }
